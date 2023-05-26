bb16e74c9e85b5d83c9702c00897f65ed687f101
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-05-26 08:22:04 +0000

    Add Installation of Tomcat Instances.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,26 @@
 class vs_java (
-    $jdkVersion     = '17',
+    $jdkVersion         = '17',
+    $mavenVersion       = '3.9.2',
+    Hash $tomcatConfig  = {},
 ) {
-    class { '::vs_java::openjdk':
+    ################################################################################################################
+    # Add Class if Not Declared Already
+    # https://github.com/puppetlabs/puppetlabs-stdlib/blob/main/lib/puppet/parser/functions/ensure_resource.rb
+    ################################################################################################################
+    ensure_resource( 'class', 'vs_java::openjdk', {
         jdkVersion  => $jdkVersion
+    })
+    
+    Exec{ 'Set Java Default':
+        command => "/opt/vs_devenv/set_default_java.sh ${jdkVersion}",
+        require => [ Class['vs_java::openjdk'] ],
+    }
+    
+    class { 'maven::maven':
+        version => "${mavenVersion}",
+    }
+    
+    class { '::vs_java::tomcat':
+        config  => $tomcatConfig,
     }
 }
